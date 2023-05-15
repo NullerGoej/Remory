bc871cdc1505535583799b5ea209eb0df1bfcff0
NullerGoej <bastian2010@live.dk>
2023-05-15 10:52:06 +0200

    Initial tabs creation

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.remory',
+  appName: 'remory',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

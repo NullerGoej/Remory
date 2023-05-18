38b85c6bc959d18f35fafa3469a093d54bf33aed
Enitsrik <kirs525p@edu.zealand.dk>
2023-05-18 11:56:37 +0200

    ko

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import {FormBuilder, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,ReactiveFormsModule]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+loginForm!: FormGroup; 
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {this.loginForm=this.formBuilder.group({email:["",[Validators.required]] ,password:["",[Validators.required]]});}
+
+  saveChanges() {console.log(this.loginForm.value);
   }
+
 
 }

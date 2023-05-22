913cd4fd2e03997f28a86dd2378ee23130093248
Enitsrik <kirs525p@edu.zealand.dk>
2023-05-22 10:29:29 +0200

    uf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
 ];
